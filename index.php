aa9780ee2fb665914a8ae0f7d353ac417e27567b
FrancescoPerez <114742429+FrancescoPerez@users.noreply.github.com>
2022-10-26 09:07:26 +0200

    Correzione della struttura file, inizio script PHP

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
+<?php 
+
+?>
+
+
 <html>
 <head>
      <link rel="stylesheet" href="style.css">
 </head>
  <body>
-    <h1> Quanto sarai povero?</h1>
+    <h1>Quanto sarai povero?</h1>
 
-    <h2>giorni</h2>
-      <p1><input type="text" name="gioni"></p1>
+    <h2>Giorni</h2>
+      <p1><input type="text" name="giorni"></p1>
 
-    <h3>ore giornaliere di lavoro</h3>
+    <h3>Ore giornaliere di lavoro</h3>
     <p2><input type="text" name="lav"></p2>
 
     <h4>Compenso orario</h4>
